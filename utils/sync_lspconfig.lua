9976f7fb728f760f9fd86ece5048f82ee6315c03
Luca Saccarola <github.e41mv@aleeas.com>
2024-11-09 20:59:51 +0100

    Make output of lua script more reliable
    Mainly use more a functional approac and use a ton of `vim.deepcopy`

--- utils/sync_lspconfig.lua
+++ utils/sync_lspconfig.lua
@@ -1,59 +1,94 @@
-local path = vim.fn.stdpath('data') .. "/site/pack/deps/start/nvim-lspconfig"
-local config_path = path .. "/lua/lspconfig/configs"
-
 local function strip_functions(tbl)
-    for k, v in pairs(tbl) do
-        if type(v) == "function" then
-            tbl[k] = nil
-        elseif type(v) == "table" and not vim.isarray(v) then
-            strip_functions(v)
-        end
-    end
+	for k, v in pairs(tbl) do
+		if type(v) == "function" then
+			tbl[k] = nil
+		elseif type(v) == "table" and not vim.isarray(v) then
+			strip_functions(v)
+		end
+	end
 end
 
-local function server_specific_config(name, config)
-    if name == "rust-analyzer" then
-        config.syncInit = true
-    end
+local function get_server_configs()
+	local lspconfig_path = vim.fn.stdpath("data") .. "/site/pack/deps/start/nvim-lspconfig"
+	local server_configs_path = lspconfig_path .. "/lua/lspconfig/configs"
+
+	local server_configs = {}
+	for filename, filetype in vim.fs.dir(server_configs_path) do
+		if filetype == "file" then
+			local server_name = filename:match("(.+)%..+$")
+			local server_definition = require("lspconfig.configs." .. server_name)
+			local server_config = vim.deepcopy(server_definition.default_config)
+			strip_functions(server_config)
+			server_configs[server_name] = server_config
+		end
+	end
+
+	return vim.deepcopy(server_configs)
 end
 
-local function normalize_server_config(name, config)
-    strip_functions(config)
-    server_specific_config(name, config)
+local function normalize_server_config(server_name, server_config)
+	local new_config = vim.deepcopy(server_config)
 
-    local config = config.default_config
-    for k, v in pairs(config) do
-        if k == "cmd" then
-            config.path = config[k][1]
-            table.remove(config[k], 1)
-            config.args = config[k]
-            config[k] = nil
-        elseif k == "filetypes" then
-            config.filetype = config[k]
-            config[k] = nil
-        elseif k == "single_file_support" then
-            config[k] = nil
-        elseif k == "settings" then
-            config.workspaceConfig = {}
-            config.workspaceConfig.settings = config[k]
-            config[k] = nil
-        elseif k == "capabilities" then
-            config.features = config[k]
-            config[k] = nil
-        end
-    end
-    config.name = name
+	-- I do a deepcopy to making sure is not refering a table and mess up the
+	-- serialization.  This is more a sanity check for me.
+	for k, v in pairs(new_config) do
+		local old_value = vim.deepcopy(server_config[k])
 
-    return config 
+		if k == "cmd" then
+			new_config.path = old_value[1]
+			table.remove(old_value, 1)
+			new_config.args = old_value
+			new_config[k] = nil
+		elseif k == "filetypes" then
+			new_config.filetype = old_value
+			new_config[k] = nil
+		elseif k == "single_file_support" then
+			new_config[k] = nil
+		elseif k == "settings" then
+			new_config.workspaceConfig = {}
+			new_config.workspaceConfig.settings = old_value
+			new_config[k] = nil
+		elseif k == "capabilities" then
+			new_config.features = old_value
+			new_config[k] = nil
+		end
+	end
+	new_config.name = server_name
+
+	return vim.deepcopy(new_config)
 end
 
-local configs = {}
-for fname, ftype in vim.fs.dir(config_path) do
-    if ftype == "file" then
-        server = fname:match("(.+)%..+$")
-        local config = require('lspconfig.configs.' .. server)
-        configs[server] = normalize_server_config(server, config)
-    end
+local function apply_server_specific_config(server_configs)
+	server_configs["rust_analyzer"].syncInit = true
 end
 
-io.write(vim.json.encode(configs))
+local function normalize_server_configs(server_configs)
+	local configs = vim.deepcopy(server_configs)
+	for server, config in pairs(configs) do
+		configs[server] = normalize_server_config(server, config)
+	end
+	apply_server_specific_config(configs)
+	return vim.deepcopy(configs)
+end
+
+local function write_server_configs(server_configs)
+	local file = vim.uv.fs_open("servers.json", "w", 438)
+	if not file then
+		error("Couldn't open server.json")
+	end
+	local data = vim.deepcopy(server_configs)
+	local ok, result = pcall(vim.json.encode, data)
+	if not ok then
+		error(result)
+	end
+	assert(vim.uv.fs_write(file, result))
+	assert(vim.uv.fs_close(file))
+end
+
+local function main()
+	local server_configs = get_server_configs()
+	local server_configs_normalized = normalize_server_configs(server_configs)
+	write_server_configs(server_configs_normalized)
+end
+
+main()
