98972b64c0c87e13827fa95747ba78a75f428297
Luca Saccarola <github.e41mv@aleeas.com>
2024-11-17 10:16:34 +0100

    Clean up python script

--- scripts/sync_lspconfig.py
+++ scripts/sync_lspconfig.py
@@ -3,63 +3,72 @@ import subprocess
 import sys
 
 
-def panic_after_non_normalized_server(key):
-    sys.stderr.write(
-        """
-The server.json wasn't normalized properly. The invalid '{}' key was present in the configuration. 
-This is normal since lua sometimes suck to remove keys from dictionaries. Try to run the script again." 
-""".format(
-            key
-        )
-    )
-    sys.exit(-1)
+def get_server_configs():
+    process = subprocess.run(["nvim", "-l", "scripts/sync_lspconfig.lua"])
 
-
-if __name__ == "__main__":
-    result = subprocess.run(["nvim", "-l", "scripts/sync_lspconfig.lua"])
-
-    if result.returncode != 0:
+    if process.returncode != 0:
         sys.stderr.write("nvim script failed for some reason\n")
         sys.exit(-1)
 
     with open("servers.json", "r") as file:
         content = file.read()
-        configs = json.loads(content)
 
-    servers = {}
+    configs = json.loads(content)
+
+    return configs
+
+
+def generate_vim9_defs(server_configs):
     buffer = "vim9script\n\n"
-    for key, value in sorted(configs.items()):
-        name = key.replace("_", " ").replace("-", " ").title()
+
+    for server, config in sorted(server_configs.items()):
+        name = server.replace("_", " ").replace("-", " ").title()
         # Convert server names from snake case to PascalCase
         fn_name = name.replace(" ", "")
         buffer += f"export def {fn_name}(opts: dict<any> = {{}})\n"
         # Creating a local variable to the function with the correct configuration
-        config = json.dumps(value, sort_keys=True)
-        buffer += f"  var settings = {config}\n"
+        config_str = json.dumps(config, sort_keys=True)
+        buffer += f"  var settings = {config_str}\n"
         # Registering the LSP
         buffer += "  g:LspAddServer([settings->extend(opts, 'force')])\n"
         buffer += "enddef\n"
-        # Save servers names for documentation
-        servers[name] = json.dumps(value, sort_keys=True, indent=2)
 
     with open("autoload/lsp_settings.vim", "w") as f:
         f.write(buffer)
 
+
+def generate_vim_documentation(servers_configs):
     buffer = "LSP SERVER LIST"
     doc_tag = "*lsp-settings-server-list*"
     buffer += " " * (78 - len(buffer) - len(doc_tag)) + doc_tag + "\n"
     buffer += "\n"
-    for name, config in sorted(servers.items()):
+
+    for server, config in sorted(servers_configs.items()):
+        # rust_analyzer -> Rust Analyzer
+        name = server.replace("_", " ").replace("-", " ").title()
         buffer += name.upper() + " ~\n"
         buffer += "Usage: >\n"
+        # Rust Analyzer -> RustAnalyzer
         name = name.replace(" ", "")
         buffer += f"    call lsp_settings#{name}()\n"
         buffer += "<\n"
         buffer += "Defaults: >\n"
-        for line in config.split("\n"):
+
+        config_str = json.dumps(config, sort_keys=True, indent=2)
+        for line in config_str.split("\n"):
             buffer += f"    {line}\n"
+
         buffer += "\n"
+
     buffer += "vim:tw=78:ts=8:noet:ft=help:norl:\n"
 
     with open("doc/lsp_settings_servers.txt", "w") as f:
         f.write(buffer)
+
+
+if __name__ == "__main__":
+    server_configs = get_server_configs()
+
+    generate_vim9_defs(server_configs)
+
+    generate_vim_documentation(server_configs)
